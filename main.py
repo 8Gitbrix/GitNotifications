b6f0687e5830c8dc981f9e3cdf778e6ad4cc84d1
Ashwin Jeyaseelan <8gitbrix@github.com>
2022-10-14 17:43:20 +0000

    Added bot commenting

---
+++ main.py
@@ -0,0 +1,33 @@
+from flask import request
+from flask import Flask
+import requests
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def api_root():
+    return 'Hello World'
+
+
+@app.route('/pr-comment', methods=['POST'])
+def prComment():
+    if request.headers['Content-Type'] == 'application/json':
+        content = request.json
+        message = f'{content["review"]["user"]["login"]} said {content["review"]["body"]}'
+        sendComment(message)
+        return content
+
+
+def sendComment(message: str):
+    payload = '{"text": "%s"}' % message
+    print(payload)
+    response = requests.post(
+        "https://hooks.slack.com/services/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",
+        data=payload
+    )
+    print(response.text)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
